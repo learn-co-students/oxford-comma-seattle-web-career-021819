8c9baf0966ffc26721e55cd2c1c1c3c5f99437ac
Chhaian Pin <chhaianpin@hotmail.com>
2019-01-08 03:52:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if (array.length == 1)
+    return array[0]
+  elsif (array.length == 2)
+    return array.join(" and ")
+  else 
+    string = ""
+    for element in array
+      if (element != array.last)
+        string += element + ", "
+      else
+        string += "and " + element
+      end
+    end
+    return string
+  end
 end
