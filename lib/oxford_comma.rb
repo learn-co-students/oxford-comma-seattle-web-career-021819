cd3f3ec39c8022e2f5caedf4a6b0cb2789f531ba
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-25 22:24:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  else if array.length > 2
+  ending = ["and "]
+  ending.push(array.slice!(-1))
+  new_array = []
+    array.each do |fruit|
+      new_array.push(fruit + ", ")
+    end
+  return new_array.join("") + ending.join("")
+else if array.length == 1
+  return array.join("")
+end
+end
+end
 end
