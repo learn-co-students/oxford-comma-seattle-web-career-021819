105c1a625a00526bc62032488ad2fa396f45b007
Njenga Kariuki <njengak@Njengas-MacBook-Pro-2.local>
2019-01-28 18:36:11 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.length > 2
+    arr.insert(arr.length-1,("and" + " " + arr.last))
+    arr.pop
+    arr.join(", ")
+  elsif arr.length == 2
+    arr.join(" and ")
+  else
+    arr.join
+  end
 end
