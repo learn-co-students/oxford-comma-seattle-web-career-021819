7464b16d481656330f8d53a84f84f3553ed02d74
clairemuller <clairermuller@gmail.com>
2019-02-05 18:14:39 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def oxford_comma(array)
+
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    string = array[0..-2].join(", ")
+    string << ", and " + array.last
+  else
+
+  end
 
 end
